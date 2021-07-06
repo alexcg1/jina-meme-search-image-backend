1f56a2bbb3f2b47d7f8ff3dec049cd9bef39ccde
Alex C-G <alexcg@outlook.com>
2021-07-06 14:56:54 +0200

    fix(get_data): rm commented line

--- get_data.sh
+++ get_data.sh
@@ -5,6 +5,5 @@ OUTPUT_DIR="./data2"
 OUTPUT_FILENAME=$OUTPUT_DIR/memes.json
 
 mkdir -p $OUTPUT_DIR
-#wget -O data/memes.json https://github.com/alexcg1/ml-datasets/blob/master/nlp/memes/memes.json?raw=true
 
 wget -O $OUTPUT_FILENAME $URL
