d80599ece76a130980030b07579a7219c48f2591
Alex C-G <alexcg@outlook.com>
2021-07-06 10:52:53 +0200

    fix(get_data): s3 url

--- get_data.sh
+++ get_data.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-mkdir -p data
-wget -O data/memes.json https://github.com/alexcg1/ml-datasets/blob/master/nlp/memes/memes.json?raw=true
+
+URL="https://jina-examples-datasets.s3.amazonaws.com/memes/memes.json"
+OUTPUT_DIR="./data2"
+OUTPUT_FILENAME=$OUTPUT_DIR/memes.json
+
+mkdir -p $OUTPUT_DIR
+#wget -O data/memes.json https://github.com/alexcg1/ml-datasets/blob/master/nlp/memes/memes.json?raw=true
+
+wget -O $OUTPUT_FILENAME $URL
